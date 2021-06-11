a6e1aeb0b63727e1051c6faa1f5c85577fe77923
Gustav <gustav.jorlov@telenorconnexion.com>
2021-06-11 21:34:22 +0200

    all

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,6 +3,6 @@ const { do_it } = require("../index");
 
 describe("The test...", () => {
   it("should be correct", () => {
-    expect(do_it()).to.eql("GO FOR A BIKE RIDE!");
+    expect(do_it("go for a bike ride!")).to.eql("GO FOR A BIKE RIDE!");
   });
 });
